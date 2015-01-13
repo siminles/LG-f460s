b20a66e4e05229bb1872e64e3b95a8780c492d86
simin <1813765241@qq.com>
2015-01-13 21:03:31 +0800

    clean

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Aug 7 16:41:46 KST 2014"
-#define LINUX_COMPILE_BY "jeongrok.seo"
-#define LINUX_COMPILE_HOST "8LLGSBX"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
